cd7c85bcd4e1ec0f85fb06b84a4c988661973c15
somak <somak@debian.somak.com>
2024-04-28 16:13:57 +0200

    string matching, my version and the book's

---
+++ cch4/print_line.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+#include<string.h>
+#define MAXSIZE 500
+/*Prints only those lines that contain the pattern given
+ * the lines shouldn't be too long*/
+int	is_pattern_present(char[], char[], int); 
+int getLine(char[]);
+int strindex(char[], char[]);
+
+char pattern[] = "ould";
+int main() {
+	char line[MAXSIZE];
+	while (getLine(line)) {
+		/*if (is_pattern_present(line, pattern, strlen(line))
+			printf("%s", line);
+		*/
+		if (strindex(line, pattern)>= 0) {
+			printf("%s", line);	
+		}
+	}			
+	return 0;
+}
+	
+int getLine(char line[]) {
+	int i, c;
+	for (i = 0; (c = getchar()) != '\n' && c != '\0' && c != EOF; i++) {
+		line[i]  = c;
+	}
+	if (c == '\n') line[i++] = c;
+	line[i] = '\0';
+	return i;
+}
+
+int is_pattern_present(char line[], char pattern[], int size) {
+	int i, c;
+	i = 0;
+	while ((c = line[i]) != '\0') {
+		if ( c == pattern[0]) {
+			int k = 0;
+			while (k < strlen(pattern) && i < size
+					 && pattern[++k] == line[++i]) {
+						;
+			}
+			if (k == strlen(pattern)) {
+				return 1;
+			} 
+		}
+		i++;
+	} return 0;
+}
+
+int strindex(char line[], char pattern[]) {
+	int i, j, k, c;
+	for (i = 0; line[i] != '\0'; i++) {
+		for (j = i, k = 0; line[j] == pattern[k]; k++, j++){
+			;
+		} if (k > 0 && pattern[k] == '\0') return i;
+	
+	}
+	return -1;
+
+}	
